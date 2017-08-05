560dc760998920b0a35683b7c9a81d48def95341
c8112002 <c8112002@gmail.com>
2017-08-05 22:29:02 +0900

    Use Kotlin

---
+++ app/src/main/java/com/example/c8112002/blenfcsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.c8112002.blenfcsample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
